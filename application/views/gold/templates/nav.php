6ca1c3bc9eec3b9773d4233a0a99483b7d011ddd
ivy jean <diligivyjean@gmail.com>
2016-03-01 17:29:54 +0800

    -add on nav bar the public release all

--- application/views/gold/templates/nav.php
+++ application/views/gold/templates/nav.php
@@ -147,6 +147,9 @@
                             <li>
                                 <a href="<?php echo base_url() . $version; ?>/publicreleaseedit">Edit Previous Releases</a>
                             </li>
+                             <li>
+                                <a href="http://www.dewslandslide.com/ajax/publicreleaseall2.php">Public Release All</a>
+                            </li>
                         </ul>
                     </li>
                     
