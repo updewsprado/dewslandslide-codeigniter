ecc492dfa9ba8548bf1a38fae98dded4d1f8fae9
Kevin Dhale dela Cruz <kevindhaledelacruz@gmail.com>
2016-06-22 14:02:07 +0800

    - Added reload every 30 minutes if no activity

--- application/views/gold/monitoring_dashboard.php
+++ application/views/gold/monitoring_dashboard.php
@@ -337,6 +337,24 @@
 
 <script>
 
+	/*
+	 * Refresh page every 30 minutes
+	 */
+	
+	var time = new Date().getTime();
+	$(document.body).bind("mousemove keypress", function(e) {
+	    time = new Date().getTime();
+	});
+
+	function refresh() {
+	    if(new Date().getTime() - time >= 60000) 
+	        window.location.reload(true);
+	    else 
+	        setTimeout(refresh, 300000);
+	}
+
+	setTimeout(refresh, 300000);
+
 	/**
 	 *	Fill the remaining empty space with <br>
 	**/
