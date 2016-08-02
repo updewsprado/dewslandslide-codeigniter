109f68faf4524e4178e4978db29541297d8ae8dd
ivy jean <diligivyjean@gmail.com>
2016-08-02 15:36:45 +0800

    -ground measurement model/controller/view .. updated name in rain

--- application/controllers/gold.php
+++ application/controllers/gold.php
@@ -296,18 +296,15 @@ class Gold extends CI_Controller {
 				break;
 			
 			case 'GroundMeas':
-					$this->load->model('Alert_model');	
+				$this->load->model('gndforms_model');	
 			
 				$data['site'] = $site;
-				$data['node'] = $node;
-				$data['datefrom'] = $datefrom;
-				$data['dateto'] = $dateto;
-
+				
 				//Data for Alert Map
 				if ($site) {
-					$data['nodeAlerts'] = $this->Alert_model->getSingleAlert($site);
-					$data['siteMaxNodes'] = $this->Alert_model->getSingleMaxNode($site);
-					$data['nodeStatus'] = $this->Alert_model->getSingleNodeStatus($site);						
+					$data['nodeAlerts'] = $this->gndforms_model->getSingleAlert($site);
+					$data['siteMaxNodes'] = $this->gndforms_model->getSingleMaxNode($site);
+					$data['nodeStatus'] = $this->gndforms_model->getSingleNodeStatus($site);						
 				}
 				else {
 					$data['nodeAlerts'] = 0;
@@ -323,34 +320,6 @@ class Gold extends CI_Controller {
 				
 				break;
 
-			case 'crackform':
-					$this->load->model('Alert_model');	
-			
-				$data['site'] = $site;
-				$data['node'] = $node;
-				$data['datefrom'] = $datefrom;
-				$data['dateto'] = $dateto;
-
-				//Data for Alert Map
-				if ($site) {
-					$data['nodeAlerts'] = $this->Alert_model->getSingleAlert($site);
-					$data['siteMaxNodes'] = $this->Alert_model->getSingleMaxNode($site);
-					$data['nodeStatus'] = $this->Alert_model->getSingleNodeStatus($site);
-											
-				}
-				else {
-					$data['nodeAlerts'] = 0;
-					$data['siteMaxNodes'] = 0;
-					$data['nodeStatus'] = 0;						
-				}				
-				
-				$data['showplots'] = 'redirectcrackPlots(this.form)';
-				$data['showdateplots'] = "showAccel(getMainForm())";
-				
-				$data['dropdown_chart'] = 'class="active"';
-				$page = 'crackform';
-				
-				break;
 
 			default:
 				break;
