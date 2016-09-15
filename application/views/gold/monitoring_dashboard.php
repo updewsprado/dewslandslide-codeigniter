6b391277e9a5752e50f414a9b30319b820b83d7a
jgeliberte <gelibertejohn@gmail.com>
2016-09-15 11:16:54 +0800

    Bug fix: Footer not being included

--- application/views/gold/monitoring_dashboard.php
+++ application/views/gold/monitoring_dashboard.php
@@ -341,6 +341,7 @@
     								<textarea style="resize:none" name="constructed-ewi" id="constructed-ewi-amd" cols="30" rows="10" class="form-control"></textarea>
     								<button type="button" id="send-btn-ewi-amd" class="btn btn-success" onclick="templateSendViaAMD()">Send</button>
     								<input type="hidden" name="site_abbr" id="site-abbr">
+                                    <input type="hidden" name="footer_ewi" id="footer-ewi">
     							</div>
     						</div>  
     					</div>
