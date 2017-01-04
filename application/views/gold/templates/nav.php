eff59069e1d11a0214f46511a06bd91d82dd3a3a
Kevin Dhale dela Cruz <kevindhaledelacruz@gmail.com>
2017-01-04 14:08:29 +0800

    - Integrated the bulletin viewing on public release individual view (bulletin-view and -editor now a backup) - Added the send to mail and download functionality on individual view - Bug fixes on dashboard latest candidate triggers and releases - Commented out placehold.it on nav due to network problem

--- application/views/gold/templates/nav.php
+++ application/views/gold/templates/nav.php
@@ -22,7 +22,7 @@
                             <a href="#">
                                 <div class="media">
                                     <span class="pull-left">
-                                        <img class="media-object" src="http://placehold.it/50x50" alt="">
+                                        <!-- <img class="media-object" src="http://placehold.it/50x50" alt=""> -->
                                     </span>
                                     <div class="media-body">
                                         <h5 class="media-heading"><strong><?php echo $first_name; ?></strong>
@@ -37,7 +37,7 @@
                             <a href="#">
                                 <div class="media">
                                     <span class="pull-left">
-                                        <img class="media-object" src="http://placehold.it/50x50" alt="">
+                                        <!-- <img class="media-object" src="http://placehold.it/50x50" alt=""> -->
                                     </span>
                                     <div class="media-body">
                                         <h5 class="media-heading"><strong><?php echo $first_name; ?></strong>
@@ -52,7 +52,7 @@
                             <a href="#">
                                 <div class="media">
                                     <span class="pull-left">
-                                        <img class="media-object" src="http://placehold.it/50x50" alt="">
+                                        <!-- <img class="media-object" src="http://placehold.it/50x50" alt=""> -->
                                     </span>
                                     <div class="media-body">
                                         <h5 class="media-heading"><strong><?php echo $first_name; ?></strong>
