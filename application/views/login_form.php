0f4b567ebb487124104d998b488abce10cb641e1
jgeliberte <gelibertejohn@gmail.com>
2017-01-16 17:05:40 +0800

    Fixed login session issue

--- application/views/login_form.php
+++ application/views/login_form.php
@@ -1,10 +1,14 @@
 <div id="login_form">
 	<h1>Login, User!</h1>
 	<?php
-	echo form_open('lin/validate_credentials');
-	echo form_input('username', 'Username');
-	echo form_password('password', 'Password');
-	echo form_submit('submit', 'Login');
-	//echo anchor('lin/signup', 'Create Account');
+	if (($this->session->userdata('first_name') != "" || is_null($this->session->userdata('first_name'))) && ($this->session->userdata('last_name') != "" || is_null($this->session->userdata('last_name')))) {
+		header( "Location: ../gold/" );
+	} else {
+		echo form_open('lin/validate_credentials');
+		echo form_input('username', 'Username');
+		echo form_password('password', 'Password');
+		echo form_submit('submit', 'Login');
+		//echo anchor('lin/signup', 'Create Account');
+	}
 	?>
 </div>
