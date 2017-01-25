fd1971701d4ad75d6def36d06027930b8c91223b
Upitdc Prado <upitdc.prado@gmail.com>
2017-01-25 10:50:16 +0800

    - templates/nav.php, fixed indentation of HTML tags

--- application/views/templates/nav.php
+++ application/views/templates/nav.php
@@ -13,19 +13,22 @@ composition using the controller
     <div id="wrapper">
 
         <!-- Navigation -->
+        
         <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+            <a  class=" btn btn-dark  btn-lg toggle" id="menu-toggle"> <i class="fa fa-bars"></i> </a>
             <!-- Brand and toggle get grouped for better mobile display -->
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand" href="<?php echo base_url() . $version; ?>/">DEWS Landslide <?php echo $version; ?></a>
-            </div>
+
+            <a class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse" style="position:fixed;left:7px">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </a>
+
             <!-- Top Menu Items -->
+
             <ul class="nav navbar-right top-nav">
+                <a class="navbar-brand navbar-brand-centered" href="<?php echo base_url() . $version; ?>/">DEWS Landslide <?php echo $version; ?></a>
                 <li class="dropdown">
                     <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-envelope"></i> <b class="caret"></b></a>
                     <ul class="dropdown-menu message-dropdown">
@@ -33,10 +36,10 @@ composition using the controller
                             <a href="#">
                                 <div class="media">
                                     <span class="pull-left">
-                                        <img class="media-object" src="http://placehold.it/50x50" alt="">
+                                        <!-- <img class="media-object" src="http://placehold.it/50x50" alt=""> -->
                                     </span>
                                     <div class="media-body">
-                                        <h5 class="media-heading"><strong>Prado Arturo</strong>
+                                        <h5 class="media-heading"><strong><?php echo $first_name; ?></strong>
                                         </h5>
                                         <p class="small text-muted"><i class="fa fa-clock-o"></i> Yesterday at 4:32 PM</p>
                                         <p>Lorem ipsum dolor sit amet, consectetur...</p>
@@ -48,10 +51,10 @@ composition using the controller
                             <a href="#">
                                 <div class="media">
                                     <span class="pull-left">
-                                        <img class="media-object" src="http://placehold.it/50x50" alt="">
+                                        <!-- <img class="media-object" src="http://placehold.it/50x50" alt=""> -->
                                     </span>
                                     <div class="media-body">
-                                        <h5 class="media-heading"><strong>Prado Arturo</strong>
+                                        <h5 class="media-heading"><strong><?php echo $first_name; ?></strong>
                                         </h5>
                                         <p class="small text-muted"><i class="fa fa-clock-o"></i> Yesterday at 4:32 PM</p>
                                         <p>Lorem ipsum dolor sit amet, consectetur...</p>
@@ -63,10 +66,10 @@ composition using the controller
                             <a href="#">
                                 <div class="media">
                                     <span class="pull-left">
-                                        <img class="media-object" src="http://placehold.it/50x50" alt="">
+                                        <!-- <img class="media-object" src="http://placehold.it/50x50" alt=""> -->
                                     </span>
                                     <div class="media-body">
-                                        <h5 class="media-heading"><strong>Prado Arturo</strong>
+                                        <h5 class="media-heading"><strong><?php echo $first_name; ?></strong>
                                         </h5>
                                         <p class="small text-muted"><i class="fa fa-clock-o"></i> Yesterday at 4:32 PM</p>
                                         <p>Lorem ipsum dolor sit amet, consectetur...</p>
@@ -107,7 +110,7 @@ composition using the controller
                     </ul>
                 </li>
                 <li class="dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-user"></i> Prado Arturo <b class="caret"></b></a>
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-user"></i> <?php echo $first_name; ?> <b class="caret"></b></a>
                     <ul class="dropdown-menu">
                         <li>
                             <a href="#"><i class="fa fa-fw fa-user"></i> Profile</a>
@@ -120,69 +123,94 @@ composition using the controller
                         </li>
                         <li class="divider"></li>
                         <li>
-                            <a href="#"><i class="fa fa-fw fa-power-off"></i> Log Out</a>
+                            <a href="logout"><i class="fa fa-fw fa-power-off"></i> Log Out</a>
                         </li>
                     </ul>
                 </li>
             </ul>
             <!-- Sidebar Menu Items - These collapse to the responsive navigation menu on small screens -->
-            <div class="collapse navbar-collapse navbar-ex1-collapse">
-                <ul class="nav navbar-nav side-nav">
-                	<li <?php echo $monitoring; ?> >
-                        <a href="<?php echo base_url() . $version; ?>/monitoring"><i class="fa fa-fw fa-th"></i> Monitoring</a>
-                    </li>
-                    <li <?php echo $dropdown_chart; ?> >
-                        <a href="javascript:;" data-toggle="collapse" data-target="#dropdown_chart"><i class="fa fa-fw fa-bar-chart-o"></i> Visual Charts <i class="fa fa-fw fa-caret-down"></i></a>
-                        <ul id="dropdown_chart" class="collapse">
-                            <li>
-                                <a href="<?php echo base_url() . $version; ?>/site">Site Level</a>
-                            </li>
-                            <li>
-                                <a href="<?php echo base_url() . $version; ?>/node">Node Level</a>
-                            </li>
-                        </ul>
-                    </li>
-                    
-                    <li></li>
-                    
-                    <FORM id="formGeneral">
-                    <li>
-	                    <div class="form-group">
-	                        <label>Site:</label>
-	                        <select class="form-control" name="sitegeneral" id="sitegeneral" onchange="<?php echo $showplots; ?>">
-	                        </select>
-	                    </div>
-                    </li>
-                    
-                    <li>
-	                    <div id="nodeGeneral" class="form-group">
-	                        <label>Node:</label>
-	                        <input class="form-control" name="node" id="node" onchange="<?php echo $showplots; ?>" type="number" min="1" max="41" value="" maxlength="2" size="2">
-	                    </div>
-                    </li>
-                    
-                    <li>
-                        <div class="form-group">
-                            <label>Database:</label>
-	                        <select class="form-control" name="dbase" id="dbase">
-		                        <option value="senslopedb">Raw</option>
-								<option value="senslopedb_purged">Purged</option>
-	                        </select>
-                        </div>						
-                    </li>
-                    </FORM>  
-                    
-                    <FORM id="formDate">
-                    <li>	
-                    	<div class="form-group">
-                            <label>Date:</label><br />                              
-		                    Start: <input type="text" id="datepicker" name="dateinput" onchange="<?php echo $showdateplots; ?>" size="10"/><br />  
-		                     End: <input type="text" id="datepicker2" name="dateinput2" onchange="<?php echo $showdateplots; ?>" size="10"/>
-	                     </div>	
-                    </li> 
-                    </FORM>
-                </ul>
-            </div>
-            <!-- /.navbar-collapse -->
-        </nav>
+            <div id="sidebar-wrapper">
+                <!-- <ul class="sidebar-nav"> -->
 
+                <div class="collapse navbar-collapse navbar-ex1-collapse">
+                    <ul class="nav navbar-nav side-nav">
+                        <li <?php echo $monitoring; ?> >
+                             <a href="<?php echo base_url() . $version; ?>/monitoring"><i class="fa fa-fw fa-th"></i> Sensor Monitoring</a>
+                        </li>
+                        <li>
+                             <a href="<?php echo base_url() . $version; ?>/monitoring_dashboard"><i class="fa fa-fw fa-exclamation-triangle"></i> Alert Monitoring</a>
+                        </li>
+                        <li>
+                            <a href="javascript:;" data-toggle="collapse" data-target="#dropdown_chatterbox"><i class="fa fa-fw fa-comment"></i> ChatterBox Menu<i class="fa fa-fw fa-caret-down"></i></a>
+                            <ul id="dropdown_chatterbox" class="collapse">
+                                <li>
+                                    <a href="<?php echo base_url() . $version; ?>/chatterbox"><i class="fa fa-fw fa-comment"></i> Chatter Box <i class="text-warning">*NEW*</i> </a>
+                                </li>
+                                <li>
+                                    <a href="<?php echo base_url() . $version; ?>/responsetracker"><i class="fa fa-fw fa-bar-chart-o"> </i>Response Tracker <i class="text-warning">*BETA*</i></a>
+                                </li>
+                            </ul>
+                        </li>
+                        <li <?php echo $dropdown_chart; ?> >
+                            <a href="javascript:;" data-toggle="collapse" data-target="#dropdown_chart"><i class="fa fa-fw fa-bar-chart-o"></i> Visual Charts <i class="fa fa-fw fa-caret-down"></i></a>
+                            <ul id="dropdown_chart" class="collapse">
+                                <li>
+                                    <a href="<?php echo base_url() . $version; ?>/site">Site Level</a>
+                                </li>
+                                <li>
+                                    <a href="<?php echo base_url() . $version; ?>/node">Node Level</a>
+                                </li>
+                                <li>
+                                 <a href="<?php echo base_url() . $version; ?>/GroundMeas"> Ground Measurement</a>
+                            	</li>
+                            	<li>
+                                      <a href="<?php echo base_url() . $version; ?>/chartlist">Sub-Surface Analysis</a>
+                                </li>
+                            </ul>
+                        </li>
+                        <li <?php echo $reportevent; ?> >
+                            <a href="<?php echo base_url() . $version; ?>/nodereport"><i class="fa fa-fw fa-list-alt"></i> Report Node Status</a>
+                        </li>
+                        <li <?php echo $reportevent; ?> >
+                            <a href="<?php echo base_url() . $version; ?>/publicrelease/faq"><i class="fa fa-fw fa-question-circle"></i> Monitoring Primer And FAQs</a>
+                        </li>
+                        <li <?php echo $reportevent; ?> >
+                            <a href="javascript:;" data-toggle="collapse" data-target="#dropdown_public_release"><i class="fa fa-fw fa-bar-chart-o"></i> Early Warning Release <i class="fa fa-fw fa-caret-down"></i></a>
+                            <ul id="dropdown_public_release" class="collapse">
+                                <li>
+                                    <a href="<?php echo base_url() . $version; ?>/publicrelease">Release Early Warning Announcement</a>
+                                </li>
+                                <li>
+                                    <a href="<?php echo base_url() . $version; ?>/publicrelease/event/all">View All Monitoring Events</a>
+                                </li>
+                            </ul>
+                        </li>
+                        <li <?php echo $reportevent; ?> >
+                            <a href="javascript:;" data-toggle="collapse" data-target="#dropdown_accomplishment"><i class="fa fa-fw fa-bar-chart-o"></i> Accomplishment Report <i class="fa fa-fw fa-caret-down"></i></a>
+                            <ul id="dropdown_accomplishment" class="collapse">
+                                <li>
+                                    <a href="<?php echo base_url() . $version; ?>/accomplishmentreport">File Accomplishment Report</a>
+                                </li>
+                                <li>
+                                    <a href="<?php echo base_url() . $version; ?>/accomplishmentreport/all">View All Accomplishment Reports</a>
+                                </li>
+                            </ul>
+                        </li>
+                        <li <?php echo $reportevent; ?> >
+                            <a href="javascript:;" data-toggle="collapse" data-target="#dropdown_site_maintenance"><i class="fa fa-fw fa-bar-chart-o"></i> Site Maintenance Report <i class="fa fa-fw fa-caret-down"></i></a>
+                            <ul id="dropdown_site_maintenance" class="collapse">
+                                <li>
+                                    <a href="<?php echo base_url() . $version; ?>/sitemaintenancereport">File Site Maintenance Report</a>
+                                </li>
+                                <li>
+                                    <a href="<?php echo base_url() . $version; ?>/sitemaintenancereport/all">View All Site Maintenance Reports</a>
+                                </li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+
+
+             <!-- /.navbar-collapse -->
+        </nav>
