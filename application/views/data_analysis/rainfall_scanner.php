b90ffabc62ee779efc6ea7d2d6ea66a6b43651f1
ivy jean <diligivyjean@gmail.com>
2017-03-06 02:19:45 +0800

    -fixed minor bug on site analysis

--- application/views/data_analysis/rainfall_scanner.php
+++ application/views/data_analysis/rainfall_scanner.php
@@ -3,14 +3,15 @@
 <link rel="stylesheet" href="http://kevinduong.net/boost/assets/css/bootstrap.css">
 <link rel="stylesheet" href="http://kevinduong.net/boost/assets/css/slider.css">
 
+
 <script src="https://code.highcharts.com/highcharts.js"></script>
 <script src="/js/third-party/exporting.js"></script>
-<script src="/js/dewslandslide/data_analysis/rainfall_scanner.js"></script>
 <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDHuLBzeBR6eA_z70NOabE-uov9jg46Azc"></script>
 <script src="/js/third-party/bootstrap-select.min.js"></script>
 <script src="http://kevinduong.net/boost/assets/js/jquery-2.1.0.js"></script>
 <script src="http://kevinduong.net/boost/assets/js/bootstrap.min.js"></script>
 <script src="http://kevinduong.net/boost/assets/js/bootstrap-slider.js"></script>
+<script src="/js/dewslandslide/data_analysis/rainfall_scanner.js"></script>
 <script>
 $(function(){
    $('#ex1').slider({
@@ -55,9 +56,73 @@ $(function(){
       <br>
       <small id="small_header"><a >&nbsp;Rainfall Scanner Page</a></small>
       <hr>
-      <div id="container" style="min-width: 310px; max-width: 800px; height: 400px; margin: 0 auto"></div>
+      <div id="container" style="min-width: 310px; height: 400px; margin: 0 auto"></div>
     </div>
   </div>
+<!-- <script type="text/javascript">
+  Highcharts.chart('container', {
+    chart: {
+        type: 'bar'
+    },
+    title: {
+        text: 'Monthly Average Temperature'
+    },
+    subtitle: {
+        text: 'Source: WorldClimate.com'
+    },
+    xAxis: {
+        categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+            'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+    },
+    yAxis: {
+        title: {
+            text: 'Temperature'
+        },
+        labels: {
+            formatter: function () {
+                return this.value + '°';
+            }
+        }
+    },
+    tooltip: {
+        crosshairs: true,
+        shared: true
+    },
+    plotOptions: {
+        spline: {
+            marker: {
+                radius: 4,
+                lineColor: '#666666',
+                lineWidth: 1
+            }
+        }
+    },
+    series: [{
+        name: 'Tokyo',
+        marker: {
+            symbol: 'square'
+        },
+        data: [7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, {
+            y: 26.5,
+            marker: {
+                symbol: 'url(https://www.highcharts.com/samples/graphics/sun.png)'
+            }
+        }, 23.3, 18.3, 13.9, 9.6]
+
+    }, {
+        name: 'London',
+        marker: {
+            symbol: 'diamond'
+        },
+        data: [{
+            y: 3.9,
+            marker: {
+                symbol: 'url(https://www.highcharts.com/samples/graphics/snow.png)'
+            }
+        }, 4.2, 5.7, 8.5, 11.9, 15.2, 17.0, 16.6, 14.2, 10.3, 6.6, 4.8]
+    }]
+});
+</script> -->
 
 
 
