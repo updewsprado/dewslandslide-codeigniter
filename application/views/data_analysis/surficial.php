d494ce5470bd45e218b5d3e695a7a4e5f019857c
ivy jean dilig <diligivyjean@gmail.com>
2017-03-22 13:51:55 +0800

    - bug fix ui/ux design for search tool

--- application/views/data_analysis/surficial.php
+++ application/views/data_analysis/surficial.php
@@ -1,5 +1,6 @@
 <script src="/js/third-party/highstock.js"></script>
 <script src="/js/third-party/exporting.js"></script>
+<script src="https://code.highcharts.com/highcharts-more.js"></script>
 <script type="text/javascript" src="/js/dewslandslide/data_analysis/surficial_level.js"></script>
 <link rel="stylesheet" type="text/css" href="/css/third-party/daterangepicker.css">
 <script src="/js/third-party/daterangepicker.js"></script>
@@ -12,84 +13,67 @@
       </div>
     </div>
     <div class="row">
-      <div class="col-lg-4">
-        <div class="panel panel-default">
-          <div class="panel-heading">
-            <h3 class="panel-title"><i class=""></i> Search Tool</h3>
-          </div>
-          <div class="panel-body">
-            <table class="table" id="searchtool" >
-              <tr>
-                <th> Site: </th>
-                <td><select class="form-control"  name="sitegeneral" id="sitegeneral" style=" width: auto;" ></td>
-              </tr>
-              <tr class="datetable">
-                <th class="datetable"> Date: </th>
-                <td class="datetable">  <div id="reportrange" class="pull-left form-control cols-xs-7" style="background: #fff;cursor: pointer;padding: 5px 10px;border: 1px solid #ccc;width: 226.22222px;" ;margin-bottom: 10px;">
-                  <i class=""></i>&nbsp;
-                  <span id="dateAnnotation"></span> <b class="caret"></b>
-                </td>
-              </tr>
-              <tr>
-                <th></th>
-                <td> <input id="submit"  type="button" value="Submit"  style=" width: 226.22222px;" ></td>
-              </tr>
-
-              <tr class="crack_id_form">
-                <th><br><br>Crack:</th>
-                <td>Select Crack ID for Surficial Analysis Graph<br> 
-                 <select class="form-control"  name="crackgeneral" id="crackgeneral" style=" width: auto;" placeholder="Select"></td>
-                 </tr>
-               </table>
+        <div class="col-lg-12" align="center">
+          <div class="panel panel-default">
+            <div class="panel-heading pull-left"><b>SEARCH TOOL:</b></div>
+            <div class="panel-body">
+              <form class="form-inline" id="searchtool">
+                <div class="form-group">
+                  <label for="site">Site:</label>
+                  <select class="form-control"  name="sitegeneral" id="sitegeneral" style=" width: auto;" ></select>
+                  &nbsp;&nbsp;&nbsp;
+                </div>
+                <div class="form-group">
+                  <label for="pwd">Date Range:</label>
+                  <div id="reportrange" class=" form-control cols-xs-7" style="background: #fff;cursor: pointer;padding: 5px 10px;border: 1px solid #ccc;width: 226.22222px;" ;margin-bottom: 10px;">
+                    <i class=""></i>&nbsp;
+                    <span id="dateAnnotation"></span> <b class="caret"></b></div>
+                    &nbsp;&nbsp;&nbsp;
+                  </div>
+                  <div class="form-group">
+                  <button type="button" id="submit" class="btn btn-info" style="margin-top: 0px;">Submit</button>
+                   &nbsp;&nbsp;&nbsp;
+                 </div>
+                 <div class="form-group crack_id_form"><h2 style=" margin-top: 0px; margin-bottom: 0px;">|</h2></div>
+                 <div class="form-group crack_id_form">
+                    <label id="popover_note" data-toggle="popover" title="Reminder" data-placement="bottom" data-content="Select Crack ID for Surficial Analysis Graph">Crack Id:</label>
+                    <select class="form-control crackgeneral"  name="crackgeneral" id="crackgeneral" style=" ;width: 226.22222px;" placeholder="Select"></select>
+                 </div>
+               </form>
              </div>
            </div>
          </div>
-         <div class="col-lg-8" id="analysis_charts">
-          <div class="panel-heading">
-            <ul class="nav nav-tabs" role="tablist">
-              <li class="active">
-                <a class="nav-link active" data-toggle="tab" href="#graph1" role="tab" aria-expanded="false"><i class=""></i> Velocity Analysis Graph </a>
-              </li>
-              <li class="nav-item" >
-                <a class="nav-link active" data-toggle="tab" href="#graph2" role="tab"><i class=""></i> Displacement Analysis Graph </a>
-              </li>
-            </ul>
-          </div>
-          <div class="panel panel-default">
-            <div class="tab-content">
-              <div class="tab-pane fade in active" id="graph1" role="tabpanel">
-                <div id="analysisVelocity" ></div>
-              </div>
-              <div class="tab-pane " id="graph2" role="tabpanel"> 
-                <div id="analysisDisplacement" ></div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="col-lg-12">
+          <div class="col-lg-12" align="center" id="nav-tab-container">
+         <div id="A0" class="panel panel-success panel_alert"><div class="panel-heading text-center"><strong>NO SIGNIFICANT GROUND MOVEMENT</strong></div></div>
+         <div id="A1" class="panel panel-warning panel_alert"><div class="panel-heading text-center"><strong><b> ALERT!! </b>SIGNIFICANT GROUND MOVEMENT OBSERVE IN THE LAST 24 HOURS</strong></div></div>
+         <div id="A2" class="panel panel-danger panel_alert"><div class="panel-heading text-center"><strong><b> ALERT!! </b>CRITICAL GROUND MOVEMENT OBSERVED IN THE LAST 48 HOURS</strong></div>
+         </div>
+         <div class="col-lg-12">
           <div class="panel-heading" >
             <div id="alert_div"></div>
             <ul class="nav nav-tabs" role="tablist">
-              <li class="active">
-                <a class="nav-link active" data-toggle="tab" href="#graphS1" role="tab" aria-expanded="false"><i class=""></i> Surficial Measurement  </a>
-              </li>
-              <li class="nav-item" >
+              <li class="active graphS2-li" >
                 <a class="nav-link active" data-toggle="tab" href="#graphS2" role="tab"><i class=""></i> Surficial Measurement Graph </a>
               </li>
-              <li class="nav-item" >
+              <li class="nav-item  graphS1-li">
+                <a class="nav-link active" data-toggle="tab" href="#graphS1" role="tab" aria-expanded="false"><i class=""></i> Surficial Measurement  </a>
+              </li>
+              <li class="nav-item graphS3-li" >
                 <a class="nav-link active" data-toggle="tab" href="#graphS3" role="tab"><i class=""></i> Surficial Measurement Form </a>
               </li>
-              
+              <li class="nav-item graph1-li">
+                <a class="nav-link active" data-toggle="tab" href="#graph1" role="tab" aria-expanded="false"><i class=""></i> Surficial Analysis Graph </a>
+              </li>
             </ul>
           </div>
           <div class="panel panel-default">
             <div class="tab-content">
-              <div class="tab-pane fade in active" id="graphS1" role="tabpanel">
+              <div class="tab-pane  graphS1" id="graphS1" role="tabpanel">
               </div>
-              <div class="tab-pane " id="graphS2" role="tabpanel"> 
+              <div class="tab-pane fade in active graphS2" id="graphS2" role="tabpanel"> 
                 <div id="ground_graph" ></div>
               </div>
-              <div class="tab-pane " id="graphS3" role="tabpanel"> 
+              <div class="tab-pane graphS3" id="graphS3" role="tabpanel"> 
                 <div class="panel-heading">SURFICIAL MEASUREMENT FORM</div>
                 <div class="panel-body">
                   <div id="education_fields">
@@ -112,12 +96,8 @@
                   <div class="col-sm-3 nopadding">
                     <div class="form-group">
                       <div class="input-group">
-                      <select class="form-control" id="educationDate" name="educationDate[]">
+                        <select class="form-control" id="educationDate" name="educationDate[]">
                           <option value="">Date</option>
-                          <!-- <option value="2015">2015</option>
-                          <option value="2016">2016</option>
-                          <option value="2017">2017</option>
-                          <option value="2018">2018</option> -->
                         </select>
                         <div class="input-group-btn">
                           <button class="btn btn-success" type="button"  onclick="education_fields();"> <span class="glyphicon glyphicon-plus" aria-hidden="true"></span> </button>
@@ -127,6 +107,10 @@
                   </div>
                   <input id="submit1"  type="button" value="Submit"   >
                 </div>
+              </div>
+              <div class="tab-pane graph1" id="graph1" role="tabpanel">
+                <div id="analysisVelocity" ></div>
+                <div id="analysisDisplacement" ></div>
               </div>
             </div>
           </div>
