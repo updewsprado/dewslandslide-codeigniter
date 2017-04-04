85f29240bc38381d2be86fd18aac2d782d2db9f3
ivy jean dilig <diligivyjean@gmail.com>
2017-04-04 12:00:51 +0800

    - node level with gintag form

--- application/views/data_analysis/node.php
+++ application/views/data_analysis/node.php
@@ -97,4 +97,29 @@
         </div>
     </div>
 </div>
-
+<div class="modal fade" id="annModal" role="dialog">
+    <div class="modal-dialog modal-sm">
+        <div class="modal-content">
+            <div class="modal-body">
+                <button type="button" class="close" data-dismiss="modal">&times;</button>
+                <p > <h4 style="text-align: center;"> TAG FORM</h4></p>
+                <form>
+                    <div class="form-group">
+                        <label for="formGroupExampleInput">Tags</label>
+                        <input type="text" class="form-control" id="tag_ids" placeholder="Example: #AcellDrift">
+                    </div>
+                    <div class="form-group">
+                        <label for="formGroupExampleInput">Timestamp</label>
+                        <input type="text" class="form-control" id="tag_time" disabled="">
+                    </div>
+                    <div class="form-group">
+                        <label for="formGroupExampleInput2">Comment</label>
+                        <textarea class="form-control" rows="5" id="comment"></textarea>
+                    </div>
+                 </form>
+                  <button type="button" class="close" class="btn-sm" id="tag_submit">SAVE</button>
+                  <br>
+            </div>
+        </div>
+    </div>
+</div>
