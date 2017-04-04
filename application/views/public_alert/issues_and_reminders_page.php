0f98b5c798629229df71a2d8560eb24b2387c17f
Kevin Dhale dela Cruz <kevindhaledelacruz@gmail.com>
2017-04-04 16:27:56 +0800

    + Added files for monitoring issues and reminders page - Some edits to accommodate changes

---
+++ application/views/public_alert/issues_and_reminders_page.php
@@ -0,0 +1,61 @@
+<!--
+    
+     Refined by: Kevin Dhale dela Cruz
+     
+     A view form for public release reports
+     located at /application/views/public_alert
+     
+     Linked at [host]/public_alert/release_form
+     
+ -->
+
+<script type="text/javascript" src="<?php echo base_url(); ?>js/dewslandslide/public_alert/dashboard_server.js"></script>
+<script type="text/javascript" src="<?php echo base_url(); ?>js/dewslandslide/public_alert/issues_and_reminders.js"></script>
+
+<div id="page-wrapper">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <h1 class="page-header">
+                    Monitoring Issues and Reminders Page
+                </h1>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-sm-12"><button id="iar_modal_link" type="button" class="btn btn-danger">Add new issue/reminder</button></div>
+        </div>
+        <br/>
+        <div id="issues_and_reminders_panels">
+        </div>
+    </div><!-- /.container-fluid -->
+</div><!-- /#page-wrapper --> 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
