702f67e20b4bee85766b24d7dc31e63e42c0973b
John Geliberte <gelibertejohn@gmail.com>
2017-04-07 14:23:14 +0800

    Finalize UI and Functionality for Gintags Viewer

--- application/views/reports/gintags_report.php
+++ application/views/reports/gintags_report.php
@@ -1,23 +1,23 @@
-<!-- Gintag report CSS -->
-<link rel="stylesheet" type="text/css" href="/css/dewslandslide/reports/gintags_report.css">
+<link rel="stylesheet" type="text/css" href="../css/dewslandslide/public_alert/monitoring_events_all.css">
 <script src="/js/dewslandslide/reports/gintags_report.js"></script>
-
-<div class="container-fluid nav-top-margin">
-	<div class="col-md-2"></div>
-	<div class="col-md-8">
-		<div class="panel panel-primary">
-			<div class="panel-heading">General Information Tags</div>
-		    <div class="panel-body">
-		    	<table id="gintag_table" class="table table-striped table-bordered" cellspacing="0" width="100%">
-		          <thead>
-		            <tr></tr>
-		          </thead>
-		          <tfoot>
-		            <tr></tr>
-		          </tfoot>
-			    </table>
-		    </div>
-	    </div>
-	</div>
-	<div class="col-md-2"></div>
+<div id="page-wrapper">
+    <div class="container">
+        <div class="page-header">
+            <h1>DEWS-Landslide General Information Tag <small>Reference Table</small>
+            </h1>
+        </div>
+        
+        <div class="row">
+            <div class="col-md-12"><div class="table-responsive">          
+                <table class="table" id="gintag_table">
+                    <thead>
+                        <tr></tr>
+                    </thead>
+                    <tfoot>
+                        <tr></tr>
+                    </tfoot>
+                </table>
+            </div>
+        </div>
+    </div>
 </div>
