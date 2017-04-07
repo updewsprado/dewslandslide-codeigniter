46054153adbc7f9e3fb6a0dbbdf1fe9d687e94a7
John Geliberte <gelibertejohn@gmail.com>
2017-04-07 19:01:52 +0800

    Enhance UI

--- application/views/communications/chatterbox_beta.php
+++ application/views/communications/chatterbox_beta.php
@@ -11,30 +11,54 @@
 <script src="/js/third-party/typeahead.js"></script>
 <script src="/js/third-party/bootstrap-tagsinput.js"></script>
 <script src="/js/third-party/notify.min.js"></script>
+
 <img id="bg-img-chatterbox" src="../../../images/dews-l-logo.png" >
 <div class="container-fluid">
 	<div class="row nav-margin">
-		<div class="col-md-2 division">
+		<div class="col-xs-3 division">
 			<div class="panel panel-primary">
 				<div class="panel-heading">MESSAGES</div>
-				<div class="panel-body">Panel Content</div>
+				<div class="panel-body">
+					<div class="row">
+						<div class="col-xs-9">
+							<input class="form-control dropdown-input" placeholder="Type name..." data-multiple />
+						</div>
+						<div class="col-xs-3">
+							<button type="button" id="go-chat" class="btn btn-primary">Go Chat</button>
+						</div>
+					</div>
+
+					<div class="row">
+						
+					</div>
+				</div>
 			</div>
 		</div>
-		<div class="col-md-8 division">
+		<div class="col-xs-7 division">
 			<div id="convo-header" class="panel panel-success">
 				<div class="panel-heading">Sitename: [SAMPLE] Office: [Sample]</div>
 				<div class="panel-body">Panel Content</div>
 			</div>
 			<hr>
-			<div id="convo-header" class="panel panel-success">
+			<div class="panel panel-success">
 				<div class="panel-body">
 					<div class="form-group">
 						<textarea class="form-control" rows="5" id="#"></textarea>
 					</div>
+
+					<div class="form-group">
+						<div class="col-xs-6 left-content">
+							<a href="">Load Message Templates</a>
+						</div>
+
+						<div class="col-xs-6 right-content">
+							<button type="button" class="btn btn-primary">Send</button>
+						</div>
+					</div>
 				</div>
 			</div>
 		</div>
-		<div class="col-md-2 division">
+		<div class="col-xs-2 division">
 			<div class="panel panel-primary">
 				<div class="panel-heading">OPTIONS</div>
 				<div class="panel-body">Panel Content</div>
