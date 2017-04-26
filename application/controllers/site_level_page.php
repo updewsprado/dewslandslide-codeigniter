39351505ac027e564730e8b747e843dd5f1809ec
ivy jean dilig <diligivyjean@gmail.com>
2017-04-26 17:06:04 +0800

    - updated link for node report and edit the logout of site_analysis_page.php and Site_level_page

--- application/controllers/site_level_page.php
+++ application/controllers/site_level_page.php
@@ -10,7 +10,7 @@ class Site_level_page extends CI_Controller {
 
 	public function index()
 	{
-
+		$this->is_logged_in();
 		$page = 'Column Level';
 		$data['first_name'] = $this->session->userdata('first_name');
 		$data['last_name'] = $this->session->userdata('last_name');
@@ -24,6 +24,17 @@ class Site_level_page extends CI_Controller {
 		$this->load->view('data_analysis/column', $data);
 	}
 
+	public function is_logged_in() {
+		$is_logged_in = $this->session->userdata('is_logged_in');
+		
+		if(!isset($is_logged_in) || ($is_logged_in !== TRUE)) {
+			echo 'You don\'t have permission to access this page. <a href="../lin">Login</a>';
+			die();
+		}
+		else {
+		}
+	}
+	
 	public function getAllSiteNames(){
 		$result = $this->site_level_model->getSiteNames();
 		print json_encode($result);
