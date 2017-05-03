90fe395abcb13c3f31f1ccee04abbd16bf7f382d
ivy jean dilig <diligivyjean@gmail.com>
2017-05-03 16:24:36 +0800

    - testing analysis chart downloader

--- application/views/data_analysis/site_analysis.php
+++ application/views/data_analysis/site_analysis.php
@@ -276,6 +276,24 @@
     </div>
   </div>
 </div>
-
+<div class="modal fade" id="pdfModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+        <h4 class="modal-title" id="myModalLabel">Modal title</h4> 
+      </div>
+      <div class="modal-body">
+        <div style="text-align: center;" id="pdfsvg">
+          <iframe src="/temp/charts_render/compiled.pdf" frameborder="0"></iframe>
+        </div>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
 
 
