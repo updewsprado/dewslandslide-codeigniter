9fd3ffef2dd0eb85fe6bec200a038bc52a089326
ivy jean dilig <diligivyjean@gmail.com>
2017-05-03 17:20:12 +0800

    - final testing  analysis pdf download

--- application/views/data_analysis/site_analysis.php
+++ application/views/data_analysis/site_analysis.php
@@ -276,24 +276,23 @@
     </div>
   </div>
 </div>
-<div class="modal fade" id="pdfModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
-        <h4 class="modal-title" id="myModalLabel">Modal title</h4> 
-      </div>
-      <div class="modal-body">
-        <div style="text-align: center;" id="pdfsvg">
-          <iframe src="/temp/charts_render/compiled.pdf" frameborder="0"></iframe>
+<div class="modal fade" id="pdfModal" role="dialog">
+    <div class="modal-dialog modal-lg">
+      <div class="modal-content">
+        <div class="modal-header">
+          <button type="button" class="close" data-dismiss="modal">&times;</button>
+          <h4 class="modal-title">Unified Single Attachment</h4>
         </div>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
+        <div class="modal-body">
+          <div style="text-align: center;" id="pdfsvg">
+        <iframe src="/temp/charts_render/compiled.pdf" frameborder="0" style="width:1000px; height:500px;"></iframe>
+        </div>
+        </div>
+        <div class="modal-footer">
+          <a href="/temp/charts_render/compiled.pdf" download="" id="renamePdf"><button type="button" class="btn btn-default" id="downloadPDF">Download</button></a>
+        </div>
       </div>
     </div>
   </div>
 </div>
-
 
