5415229aa33f16914bbac6762bdf44c29af0f5d0
John Geliberte <gelibertejohn@gmail.com>
2017-06-05 11:56:25 +0800

    Refactor automated insertion to narrative for No ack

--- application/models/narrative_automation_model.php
+++ application/models/narrative_automation_model.php
@@ -19,8 +19,17 @@ class Narrative_automation_model extends CI_Model {
 	}
 
 	public function fetchMessagesFromLastRelease($data) {
-		$query = "SELECT * FROM narratives WHERE timestamp <= '".$data['current_release_time']."' AND timestamp >= '".$data['last_release_time']."' AND event_id='".$data['event_id']."'";
-		$result = $this->db->query($query);
-		return $result->result();
+		$onset_array = [];
+		$onset_query = "SELECT public_alert_release.event_id,public_alert_release.data_timestamp,public_alert_event.event_start FROM public_alert_release INNER JOIN public_alert_event ON public_alert_release.event_id = public_alert_event.event_id WHERE public_alert_event.status = 'on-going' AND public_alert_event.site_id='".$data['event_id']."';";
+		$onset_result = $this->db->query($onset_query);
+		if ($onset_result->num_rows > 0) {
+			$query = "SELECT * FROM narratives WHERE timestamp <= '".$data['current_release_time']."' AND timestamp >= '".$data['last_release_time']."' AND event_id='".$data['event_id']."'";
+			$result = $this->db->query($query);
+			return $result->result();
+		} else {
+			$details['narrative'] = "Automatic Early warning information acknowledged";
+			array_push($onset_array,(object)$details);
+			return $onset_array;
+		}
 	}
 }
