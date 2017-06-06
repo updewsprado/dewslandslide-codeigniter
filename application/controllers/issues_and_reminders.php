bb75f2b8f4aa49cb9cbb3f24d0b3da76593452d8
Kevin Dhale dela Cruz <kevindhaledelacruz@gmail.com>
2017-06-06 14:28:49 +0800

    - Improvements on issues and reminders modal - Some bug fixes - Added plugin for Datatable Moment JS sorting

--- application/controllers/issues_and_reminders.php
+++ application/controllers/issues_and_reminders.php
@@ -44,6 +44,87 @@
 			return $this->load->view('public_alert/issues_and_reminders_builder', $data);
 		}
 
+		public function getAllRowsAsync()
+		{
+			$draw = $_POST["draw"];//counter used by DataTables to ensure that the Ajax returns from server-side processing requests are drawn in sequence by DataTables
+			$orderByColumnIndex  = $_POST['order'][0]['column'];// index of the sorting column (0 index based - i.e. 0 is the first record)
+			$orderBy = $_POST['columns'][$orderByColumnIndex]['data'];//Get name of the sorting column from its index
+			$orderType = $_POST['order'][0]['dir']; // ASC or DESC
+			$start  = $_POST["start"];//Paging first record indicator.
+			$length = $_POST['length'];//Number of records that the table can display in the 
+			                           //current draw
+			$extraFilter = $_POST['extra_filter'];
+			$status = $_POST['extra_filter']['status'];
+
+			$recordsTotal = $this->issues_model->getEventCount($status);
+
+			function addTableName($x)
+			{
+				switch ($x) {
+					case 'iar_id':
+					case 'ts_posted':
+					case 'detail':
+					case 'status':
+					case 'resolution':
+						$x = "issues_and_reminders." . $x;
+						break;
+					case 'posted_by':
+						$x = "issues_and_reminders.user_id";
+						break;
+					case 'name':
+					case 'id':
+						$x = "site." . $x;
+						break;
+					case 'internal_alert_level': 
+						$x = "public_alert_release." . $x;
+						break;
+				}
+
+				return $x;
+			}
+
+			$orderBy = addTableName($orderBy);
+
+			if( !empty($_POST['search']['value']) || $extraFilter['hasFilter'] != false )
+			{
+				$search = [];
+				if( $_POST['search']['value'] != '' )
+				{
+					for( $i=0; $i<count( $_POST['columns'] ); $i++ ) 
+					{
+						$x = addTableName( $_POST['columns'][$i]['data'] );
+			            $search[ $x ] = $_POST['search']['value'];
+			        }
+			    }
+		        $search = count($search) == 0 ? null : $search;
+
+		        $filter = [];
+		        // if( $extraFilter['status'] != null ) $filter[ addTableName('status') ] = $extraFilter['status'];
+		        // if( $extraFilter['site'] != null ) $filter[ addTableName('id') ] = $extraFilter['site'];
+		        $filter = count($filter) == 0 ? null : $filter;
+
+		        $recordsFiltered = $this->issues_model->getEventCount($status, $search, $filter);
+
+		        $data = $this->issues_model->getAllRowsAsync($status, $search, $filter, $orderBy, $orderType, $start, $length);
+			}
+			else {
+				$data = $this->issues_model->getAllRowsAsync($status, null, null, $orderBy, $orderType, $start, $length);
+
+				$recordsFiltered = $recordsTotal;
+			}
+
+			$response = array (
+		        "draw" => $draw,
+		        "recordsTotal" => $recordsTotal,
+		        "recordsFiltered" => $recordsFiltered,
+		        "data" => $data
+		    );
+
+			// $result = $this->issues_model->getAllRowsAsync();
+			echo json_encode($response);
+		}
+
+
 		public function archiveIssuesFromLoweredEvents()
 		{
 			$event_id = $_POST['event_id'];
@@ -111,11 +192,16 @@
 
 		public function archive()
 		{
+			$post = json_decode($_POST['data']);
+			$resolution = $post->resolution == "" ? null : $post->resolution;
+
 			$data = array(
-				'status' => "archived"
+				'status' => "archived",
+				'resolved_by' => $post->resolved_by,
+				'resolution' => $resolution
 			);
 
-			$this->issues_model->update_row("iar_id", $_POST['iar_id'], "issues_and_reminders", $data);
+			$this->issues_model->update_row("iar_id", $post->iar_id, "issues_and_reminders", $data);
 		}
 
 		public function is_logged_in() 
