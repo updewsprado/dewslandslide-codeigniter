b6cb0854ef8f5d9328048ff4dc3988a2094c3f85
John Geliberte <gelibertejohn@gmail.com>
2017-06-07 19:00:16 +0800

    Added #GroundMeasReminder and #GroundMeasReminderAck

--- application/views/communications/chatterbox_beta.php
+++ application/views/communications/chatterbox_beta.php
@@ -384,7 +384,7 @@
 	      </div>
 	      <div class="modal-body"> 
 	        <div class="alert alert-info" role="alert">
-	          <p style="padding: 0px;"><strong>New Feature!</strong> You can now tag messages in chatterbox! </br>.&nbsp &nbsp • <strong>Important Tags: </strong>#EwiMessage,#EwiResponse</p>
+	          <p style="padding: 0px;"><strong>New Feature!</strong> You can now tag messages in chatterbox! </br>.&nbsp &nbsp • <strong>Important Tags: </strong>#EwiMessage, #EwiResponse<br>&ensp; &ensp; &ensp; &ensp; &ensp; &ensp; &ensp; &ensp; &ensp; &ensp; &ensp; &ensp; &ensp;#GroundMeasReminder, #GroundMeasReminderAck</p>
 	        </div>
 	        <div class="form-group">
 	        	<input type="text" class="form-control" id="gintags" name="gintags" data-role="tagsinput" data-provide="typeahead" placeholder="E.g #EwiMessage" style="display:none" required>
