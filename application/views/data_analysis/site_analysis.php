7a548fb5ab60524c2965d04cf95bc9f876649bbb
ivy jean dilig <diligivyjean@gmail.com>
2017-07-26 16:09:22 +0800

    - update pdf alignment

--- application/views/data_analysis/site_analysis.php
+++ application/views/data_analysis/site_analysis.php
@@ -240,7 +240,7 @@
   <svg id="rainfallsvg" xmlns="http://www.w3.org/2000/svg" width="900" height ="1300"></svg>
 </div>
 <div class="col-sm-12 svgBox" id="surfSvg">
-  <svg id="surficialsvg" xmlns="http://www.w3.org/2000/svg" width="950" height ="1350"></svg>
+  <svg id="surficialsvg" xmlns="http://www.w3.org/2000/svg" width="920" height ="1350"></svg>
 </div>
 <div class="col-sm-12 svgBox" id="pzSvg">
   <svg id="piezosvg" xmlns="http://www.w3.org/2000/svg" width="900" height ="1300"></svg>
@@ -249,7 +249,7 @@
   <svg id="subsurfacesvg" xmlns="http://www.w3.org/2000/svg" width="900" height ="2600"></svg>
 </div>
 <div class="col-sm-12 svgBox" id="aceSvg">
-  <svg id="accelsvg" xmlns="http://www.w3.org/2000/svg" width="1000" height ="1300"></svg>
+  <svg id="accelsvg" xmlns="http://www.w3.org/2000/svg" width="950" height ="1350"></svg>
 </div>
 <div class="modal fade" id="groundModal" role="dialog">
   <div class="modal-dialog modal-lg">
