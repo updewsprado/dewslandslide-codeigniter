8058b334a32a4f06f1ca305d202c23fa93bc78d0
ivy jean dilig <diligivyjean@gmail.com>
2017-09-08 10:05:28 +0800

    - updated with CRUD

--- application/controllers/surficial_page.php
+++ application/controllers/surficial_page.php
@@ -63,6 +63,48 @@ class Surficial_page extends CI_Controller {
 		print json_encode($result);
 	}
 
+	public function getAllGroundMeasID(){
+		$data_result = $_POST['dataSubmit'];
+		$data["timestamp"] = $data_result["timestamp"];
+		$data["crack_id"] = $data_result["crack_id"];
+		$data["site"] = $data_result["site"];
+		$result = $this->surficial_model->getGroundMeasID($data);
+		print json_encode($result);
+	}
+
+	public function EditGroundMeas(){
+		$data_result = $_POST['dataSubmit'];
+		$data["id"] = $data_result["id"];
+		$data["timestamp"] = $data_result["timestamp"];
+		$data["crack_id"] = $data_result["crack_id"];
+		$data["site"] = $data_result["site"];
+		$data["meas"] = $data_result["meas"];
+		$data["status"] = 'edited';
+		$result = $this->surficial_model->getGroundMeas($data);
+		print $result;
+	}
+
+	public function DeleteGroundMeas(){
+		$data_result = $_POST['dataSubmit'];
+		$data["id"] = $data_result["id"];
+		$data["status"] = 'deleted';
+		$result = $this->surficial_model->getGroundMeas($data);
+		print $result;
+	}
+
+	public function AddGroundMeas(){
+		$data_result = $_POST['dataSubmit'];
+		$data["timestamp"] = $data_result["timestamp"];
+		$data["meas_type"] = $data_result["meas_type"];
+		$data["site_id"] = $data_result["site_id"];
+		$data["crack_id"] = $data_result["crack_id"];
+		$data["observer_name"] = $data_result["observer_name"];
+		$data["meas"] = $data_result["meas"];
+		$data["weather"] = $data_result["weather"];
+		$data["reliability"] = 'Y';
+		$result = $this->surficial_model->AddGroundMeas($data);
+		print $result;
+	}
 
 }
 ?>
--- application/models/surficial_model.php
+++ application/models/surficial_model.php
@@ -61,5 +61,84 @@ class surficial_model extends CI_Model {
 		return $query->result();
 	}
 
+	public function getGroundMeasID($data){
+		$timestamp = $data["timestamp"];
+		$crack = $data["crack_id"];
+		$site = $data["site"];
+		if($site == "mng"){
+			$site_name = "man";
+		}else if( $site == "png"){
+			$site_name = "pan";
+		}else if($site == "bto"){
+			$site_name = "bat";
+		}else if($site == "jor"){
+			$site_name = "pob";
+		}else{
+			$site_name = $site;
+		}
+		if($crack != "none"){
+			$sql = "SELECT * FROM gndmeas where timestamp = '$timestamp' and crack_id = '$crack'  and site_id = '$site_name'";
+		}else{
+			$sql = "SELECT * FROM gndmeas where timestamp = '$timestamp' and site_id = '$site_name'";
+		}
+		
+		$query = $this->db->query($sql);
+		return $query->result();
+	}
+
+	public function getGroundMeas($data){
+		$id = $data["id"];
+		if( $data["status"] == "deleted"){
+			$sql = "DELETE FROM `gndmeas` WHERE `id`='$id'";
+		}else if($data["status"] == "edited"){
+			$timestamp = $data["timestamp"];
+			$crack = $data["crack_id"];
+			$site = $data["site"];
+			$meas =$data["meas"];
+			if($site == "mng"){
+				$site_name = "man";
+			}else if( $site == "png"){
+				$site_name = "pan";
+			}else if($site == "bto"){
+				$site_name = "bat";
+			}else if($site == "jor"){
+				$site_name = "pob";
+			}else{
+				$site_name = $site;
+			}
+			$sql = "UPDATE `gndmeas` SET `timestamp`='$timestamp', `crack_id`='$crack', `meas`='$meas', `site_id`= '$site_name'
+			WHERE `id`='$id'";
+		}
+
+		$query = $this->db->query($sql);
+		return 'Done';	
+	}
+
+	public function AddGroundMeas($data){
+		$timestamp = $data["timestamp"];
+		$meas_type = $data["meas_type"];
+		$site = $data["site_id"];
+		$crack_id =$data["crack_id"];
+		$observer_name = $data["observer_name"];
+		$meas = $data["meas"];
+		$weather =$data["weather"];
+		$reliability = $data["reliability"];
+		if($site == "mng"){
+			$site_name = "man";
+		}else if( $site == "png"){
+			$site_name = "pan";
+		}else if($site == "bto"){
+			$site_name = "bat";
+		}else if($site == "jor"){
+			$site_name = "pob";
+		}else{
+			$site_name = $site;
+		}
+		$sql = "INSERT INTO `gndmeas` (`timestamp`, `meas_type`, `site_id`, `crack_id`, `observer_name`, `meas`, `weather`, `reliability`) VALUES ('$timestamp', '$meas_type', '$site_name', '$crack_id', '$observer_name', '$meas', '$weather', '$reliability')";
+
+		$query = $this->db->query($sql);
+		return 'Done';	
+	}
+
 	
 }
