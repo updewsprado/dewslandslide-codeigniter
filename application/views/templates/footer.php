8757aec7f8c1975ef00a20df308897a12178a3d7
John Geliberte <gelibertejohn@gmail.com>
2017-11-17 13:29:14 +0800

    Added server time

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -4,6 +4,11 @@
 		<div class="container">
 			<div class="navbar-text"></div>
 		</div>
+		<div class="container-fluid">
+			<div class="col-md-12 col-xs-12 col-sm-12" style="text-align: right; color: white;">
+				<span>Server time: </span><span id="server-time">Na-Na-Na Na:Na:Na</span>
+			</div>
+		</div>
 	</nav>
 </footer>
 
