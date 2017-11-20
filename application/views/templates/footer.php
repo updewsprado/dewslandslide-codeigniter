e3961335a79fdd27544ecccd2f79e9b62fe46334
John Geliberte <gelibertejohn@gmail.com>
2017-11-20 15:00:30 +0800

    Minor UI fix

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,11 +1,8 @@
 
 <footer class="footer">
 	<nav class="navbar navbar-inverse navbar-fixed-bottom" role="navigation">
-		<div class="container">
-			<div class="navbar-text"></div>
-		</div>
 		<div class="container-fluid">
-			<div class="col-md-12 col-xs-12 col-sm-12" style="text-align: right; color: white;">
+			<div class="col-md-12 col-xs-12 col-sm-12" style="text-align: right; color: white; margin-top: 0.5%;">
 				<span>Server time: </span><span id="server-time">Na-Na-Na Na:Na:Na</span>
 			</div>
 		</div>
