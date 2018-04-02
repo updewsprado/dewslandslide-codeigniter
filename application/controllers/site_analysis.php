5a66da81beced0509867b93453a20ea2fe6900d3
jhndvdgvr <david063095@gmail.com>
2018-04-02 09:51:54 +0800

    updated some variables

--- application/controllers/site_analysis.php
+++ application/controllers/site_analysis.php
@@ -957,7 +957,7 @@ class Site_analysis extends CI_Controller {
             $python_path = "C:/Users/Dynaslope/Anaconda2/python.exe";
             $file_path = "C:/xampp/updews-pycodes/Liaison/";
         } elseif (strpos($os, "UBUNTU") !== false || strpos($os, "Linux") !== false) {
-            $python_path = "/home/ubuntu/anaconda2/bin/python";
+            $python_path = "/home/jdguevarra/anaconda2/bin/python";
             $file_path = "/var/www/updews-pycodes/Liaison/";
         } else {
             throw new Exception("Unknown OS for execution... Script discontinued...");
