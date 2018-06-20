a022f1cacd6f0949ecbc6a7dba7d9164ef63df4a
jhndvdgvr <david063095@gmail.com>
2018-06-20 19:21:37 +0800

    added narrative modal

--- application/views/communications/chatterbox_beta.php
+++ application/views/communications/chatterbox_beta.php
@@ -901,8 +901,30 @@
     	</div>
       </div>
       <div class="modal-footer">
-        <button type="button" class="btn btn-danger" data-dismiss="modal">Reset</button>
-        <button type="button" class="btn btn-primary">Confirm</button>
+        <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary" id="confirm-tagging">Confirm</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+<!-- narrative Modal -->
+<div class="modal fade" id="narrative-modal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h4 class="modal-title">Narrative</h4>
+      </div>
+      <div class="modal-body">
+        <div class="scrollable-div">
+        	
+        	
+        </div>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary" id="save-narrative">Confirm</button>
       </div>
     </div>
   </div>
