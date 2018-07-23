3aaa8939c5fcb0637ef09f8d5d66ffb2034c59fc
John Louie Nepomuceno <jlouienepomuceno@gmail.com>
2018-07-23 14:28:41 +0800

    Created a new look for the login page of DEWS-L site. Added some minor responsiveness.

--- application/views/includes/header.php
+++ application/views/includes/header.php
@@ -1,10 +1,14 @@
 <!doctype html>
 <html lang="en">
 <head>
-	<title>untitled</title>
-	<meta http-equiv="Content-Type" content="text/html" charset="utf-8">
-	<link rel="stylesheet" href="<?php echo base_url(); ?>css/third-party/style.css" type="text/css" media="screen" title="no title" />
-	<script type="text/javascript" src="<?php echo base_url(); ?>js/third-party/jquery-1.12.4.min.js" charset="UTF-8"></script>
+    <title>Welcome to DEWS-L | Login</title>
+    <meta http-equiv="Content-Type" content="text/html" charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
 </head>
 
 <body>
