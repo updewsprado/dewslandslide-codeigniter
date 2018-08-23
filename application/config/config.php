afff8fdaccaf9701d4869026f383aeb9b27efd9b
John Geliberte <geliberteofficials@gmail.com>
2018-08-23 12:55:14 +0800

    Typo error

--- application/config/config.php
+++ application/config/config.php
@@ -17,7 +17,7 @@
 
 // $config['base_url']	= 'http://www.dewslandslide.com/';
 // $config['base_url'] = 'http://localhost/';
-$config['base_url'] = 'http://192.168.150.72/'
+$config['base_url'] = 'http://192.168.150.72/';
 
 
 $config['server_root'] = $_SERVER['DOCUMENT_ROOT'];
