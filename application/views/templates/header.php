e88962b2d53c74a8eb767170e19371ff86aaff46
Kevin Dhale dela Cruz <kevindhaledelacruz@gmail.com>
2018-09-03 14:22:59 +0800

    - changes in DEWSL-L 2.0

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -60,6 +60,6 @@
     <script type="text/javascript" src="/js/third-party/modal-reposition.js"></script>
 
     <!-- PMS SAMPLE -->
-    <script type="text/javascript" src="http://dewsl-beta.com:5053/js/pms.js"></script>
+    <script type="text/javascript" src="http://192.168.150.76:5053/js/pms.js"></script>
 
 </head>
