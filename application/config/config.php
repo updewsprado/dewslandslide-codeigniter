0e92909ee182911ab6fd66e8cc8c39f5acca4833
John Geliberte <geliberteofficials@gmail.com>
2018-09-24 16:02:30 +0800

    set db credential to x.75

--- application/config/config.php
+++ application/config/config.php
@@ -17,7 +17,9 @@
 
 // $config['base_url']	= 'http://www.dewslandslide.com/';
 $config['base_url'] = 'http://dewsl-beta.com/';
+// $config['base_url'] = 'http://192.168.150.72/';
 // $config['base_url'] = 'http://localhost/';
+
 
 
 $config['server_root'] = $_SERVER['DOCUMENT_ROOT'];
